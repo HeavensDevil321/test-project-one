114b528f45a28d3e16b282dc4ec1a6a15755e55a
Gabriel Rivera <elggrivera@gmail.com>
2015-08-29 21:01:37 -0500

    my second changes

---
+++ src/mypackage/SClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package mypackage;
+
+/**
+ * @author Carolina
+ *
+ */
+public class SClass {
+
+	/**
+	 * 
+	 */
+	public SClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
