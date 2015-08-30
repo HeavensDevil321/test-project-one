b187aeb04b8e2a23d6557a02bd5096f3cfc365a7
Gabriel Rivera <elggrivera@gmail.com>
2015-08-29 20:57:30 -0500

    adding new files

---
+++ src/mypackage/MyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mypackage;
+
+/**
+ * @author Carolina
+ *
+ */
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
